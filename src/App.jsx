aa19408c1633a36bc57f503701406e540181c034
Mac <mac@MusfiQs-MacBook-Air.local>
2024-07-08 21:54:47 +0600

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Brand from "./components/Brand/Brand";
+import WhatGPT3 from "./components/WhatGPT3/WhatGPT3";
+import Features from "./components/Features/Features";
+import Possibility from "./components/Possibility/Possibility";
+import CTA from "./components/CTA/CTA";
+import Blog from "./components/Blog/Blog";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <>
-      <h1>GPT-3</h1>
-    </>
+    <div className="app">
+      <div className="gradient__bg">        
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <CTA></CTA>
+      <Blog></Blog>
+      <Footer></Footer>
+    </div>
   );
 }
 
---
+++ src/components/CTA/CTA.jsx
@@ -0,0 +1,11 @@
+import "./CTA.css";
+
+const CTA = () => {
+  return (
+    <div>
+      <h1>Call to Action</h1>
+    </div>
+  );
+};
+
+export default CTA;
